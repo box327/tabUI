f0b0c71d3efb20618c3bfcab39cc6645296b290a
dongyoung kim <kibox327@gmail.com>
2017-01-22 14:14:09 +0900

    change desc function complete

--- tabUI.js
+++ tabUI.js
@@ -1,5 +1,5 @@
 
-function removeSelectClass(obj){
+function removeSelectedClass(obj){
   var classes = obj.className.split(" ");
   var changeClass = "";
   classes.forEach(function(iter){
@@ -9,10 +9,29 @@ function removeSelectClass(obj){
   obj.className = changeClass;
 }
 
+function sectionRedraw(obj){
+  var sectionElement = document.querySelector(".eleDisplayShow");
+  var nameElement = sectionElement.querySelector(".myName");
+  var descElement = sectionElement.querySelector(".myDesc");
+
+    nameElement.innerHTML = obj.title;
+    descElement.innerHTML = obj.body;
+}
+
+function tabAjax(url){
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", function() {
+    sectionRedraw(JSON.parse(this.responseText));
+  });;
+  oReq.open("GET", url);
+  oReq.send();
+}
+
 document.querySelectorAll(".tab").forEach(function(iter){
   iter.addEventListener("click",function(){
-    removeSelectClass(document.querySelector(".selectedTab"));
+    removeSelectedClass(document.querySelector(".selectedTab"));
     this.className += " selectedTab";
-    console.log(this.getAttribute("tab-url"));
+
+    tabAjax(this.getAttribute("tab-url"));
   });
 });

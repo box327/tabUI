c6d44f7992afc9691df74cc058ff54345852e324
dongyoung kim <kibox327@gmail.com>
2017-01-22 14:03:54 +0900

    change tab function complete

--- tabUI.js
+++ tabUI.js
@@ -1,3 +1,18 @@
-<script type="text/javascript">
 
-</script>
+function removeSelectClass(obj){
+  var classes = obj.className.split(" ");
+  var changeClass = "";
+  classes.forEach(function(iter){
+    if(iter !== "selectedTab")
+      changeClass += iter + " ";
+  });
+  obj.className = changeClass;
+}
+
+document.querySelectorAll(".tab").forEach(function(iter){
+  iter.addEventListener("click",function(){
+    removeSelectClass(document.querySelector(".selectedTab"));
+    this.className += " selectedTab";
+    console.log(this.getAttribute("tab-url"));
+  });
+});
